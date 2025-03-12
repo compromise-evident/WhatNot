5360ff83a84a01109988075236d7065aa76c9415
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2025-03-12 13:39:58 -0600

    Add files via upload

--- create_TIFF_header_file_then_append.cpp
+++ create_TIFF_header_file_then_append.cpp
@@ -2,20 +2,37 @@
 long long  width = 500; // (image  width in pixels)
 long long height = 500; // (image height in pixels)
 
-//Creates 122-byte TIFF header file, to which you may append
-//pixel values (3 bytes per pixel for red, green, and blue.)
-//It's OK to append too many bytes. Each byte must be 0-255.
-//Image viewers render TIFF pixels left-to-right, top-down.
-//Each row is (width * 3) bytes long. No compression here.
+/*Creates 122-byte TIFF header file,
+to which you may append 3 bytes per pixel:
+1st byte: intensity of red   sub-pixel.
+2nd byte: intensity of green sub-pixel.
+3rd byte: intensity of blue  sub-pixel.
+From this, you get all the colors.
+256 total intensities per sub-pixel;
+byte 0 = no power, byte 255 = full power.
+It's OK to append too many bytes. Pixels
+will be rendered left-to-right, top-down.
+Red    pixel: bytes 255,0,0
+Green  pixel: bytes 0,255,0
+Blue   pixel: bytes 0,0,255
+Black  pixel: bytes 0,0,0
+White  pixel: bytes 255,255,255
+Gray   pixel: bytes n,n,n
+Purple pixel: bytes 255,0,255
+___________________________________________________
 
-//Red    pixel: bytes 255,0,0
-//Green  pixel: bytes 0,255,0
-//Blue   pixel: bytes 0,0,255
-
-//White  pixel: bytes 255,255,255
-//Black  pixel: bytes 0,0,0
-//Grey   pixel: bytes n,n,n
-//Purple pixel: bytes 255,0,255
+Creates also 122-byte TIFF header file,
+to which you may append 1 byte per pixel:
+one intensity for red, green, and blue.
+From this, you get the full grayscale.
+256 total intensities per pixel;
+byte 0 = no power, byte 255 = full power.
+It's OK to append too many bytes. Pixels
+will be rendered left-to-right, top-down.
+Black pixel: byte 0
+White pixel: byte 255
+Gray  pixel: byte n
+*/
 
 #include <fstream>
 #include <iostream>
@@ -23,6 +40,7 @@ using namespace std;
 int main()
 {	ifstream in_stream;
 	ofstream out_stream;
+	long long temp;
 	
 	//Bytes.
 	int bytes[122] =
@@ -30,40 +48,30 @@ int main()
 		0,1,        4,0,        1,0,0,0,        0,0,0,0,        //Entry 1: width                                     10-21
 		1,1,        4,0,        1,0,0,0,        0,0,0,0,        //Entry 2: height                                    22-33
 		2,1,        4,0,        1,0,0,0,        8,0,0,0,        //Entry 3: bits per sub-pixel                        34-45
-		3,1,        4,0,        1,0,0,0,        1,0,0,0,        //Entry 4: compression                               46-57
-		6,1,        4,0,        1,0,0,0,        2,0,0,0,        //Entry 5: RGB                                       58-69
+		3,1,        4,0,        1,0,0,0,        1,0,0,0,        //Entry 4: compression (1 = none)                    46-57
+		6,1,        4,0,        1,0,0,0,        0,0,0,0,        //Entry 5: RGB/grayscale                             58-69
 		17,1,       4,0,        1,0,0,0,        122,0,0,0,      //Entry 6: byte index where pixel data begins        70-81
-		21,1,       4,0,        1,0,0,0,        3,0,0,0,        //Entry 7: sub-pixels per pixel                      82-93
+		21,1,       4,0,        1,0,0,0,        0,0,0,0,        //Entry 7: sub-pixels per pixel                      82-93
 		22,1,       4,0,        1,0,0,0,        0,0,0,0,        //Entry 8: load                                      94-105
 		23,1,       4,0,        1,0,0,0,        0,0,0,0,        //Entry 9: bytes of pixel data                      106-117
 		0,0,0,0                                                 //End                                               118-121
 	};
 	
-	//To add an entry:
-	//Add 1 to the "9".
-	//Add 12 to the "122".
-	//Place your 12-byte entry after "Entry 9".
-	//Optional: code another line as below so it can automatically apply a number to that entry.
-	
-	//Edits bytes.
-	long long temp;
+	//Edits bytes & creates file "append_to_me_3_bytes_per_pixel.tiff".
+	bytes[66] = 2; //Entry 5: RGB/grayscale
+	bytes[90] = 3; //Entry 7: sub-pixels per pixel
 	temp =  width              ; for(int a =  18; a <=  21; a++) {bytes[a] = (temp % 256); temp /= 256;} //Entry 1: width
 	temp = height              ; for(int a =  30; a <=  33; a++) {bytes[a] = (temp % 256); temp /= 256;} //Entry 2: height
 	temp = height              ; for(int a = 102; a <= 105; a++) {bytes[a] = (temp % 256); temp /= 256;} //Entry 8: load
 	temp = (width * height) * 3; for(int a = 114; a <= 117; a++) {bytes[a] = (temp % 256); temp /= 256;} //Entry 9: bytes of pixel data
+	out_stream.open("append_to_me_3_bytes_per_pixel.tiff"); for(int a = 0; a < 122; a++) {out_stream.put(bytes[a]);} out_stream.close();
 	
-	//Writes bytes to file.
-	out_stream.open("append_to_me.tiff");
-	for(int a = 0; a < 122; a++) {out_stream.put(bytes[a]);}
-	out_stream.close();
-	
-	
-	
-	
-	
-	//Sample image (remove this.)
-	out_stream.open("sample_copy_with_random_bytes_appended.tiff");
-	for(int a = 0; a <                  122; a++) {out_stream.put(    bytes[a]);}
-	for(int a = 0; a < (width * height) * 3; a++) {out_stream.put(rand() % 256);}
-	out_stream.close();
+	//Edits bytes & creates file "append_to_me_1_byte_per_pixel.tiff".
+	bytes[66] = 1; //Entry 5: RGB/grayscale
+	bytes[90] = 1; //Entry 7: sub-pixels per pixel
+	temp =  width              ; for(int a =  18; a <=  21; a++) {bytes[a] = (temp % 256); temp /= 256;} //Entry 1: width
+	temp = height              ; for(int a =  30; a <=  33; a++) {bytes[a] = (temp % 256); temp /= 256;} //Entry 2: height
+	temp = height              ; for(int a = 102; a <= 105; a++) {bytes[a] = (temp % 256); temp /= 256;} //Entry 8: load
+	temp =  width * height     ; for(int a = 114; a <= 117; a++) {bytes[a] = (temp % 256); temp /= 256;} //Entry 9: bytes of pixel data
+	out_stream.open("append_to_me_1_byte_per_pixel.tiff"); for(int a = 0; a < 122; a++) {out_stream.put(bytes[a]);} out_stream.close();
 }
