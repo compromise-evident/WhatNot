559b934078be349a67f0693fb37ed0319627eea4
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2025-03-16 14:40:21 -0600

    Add files via upload

--- mod_sequence.cpp
+++ mod_sequence.cpp
@@ -1,18 +1,16 @@
-//Run it: "apt install g++ geany". Open this in Geany. Hit F9 once. F5 to run.
+//YOUR CONTROLS:                                                                //Run it: "apt install g++ geany". Open this in Geany. Hit F9 once. F5 to run.
+unsigned long long    dividend = 1000000000000000000;
+unsigned long long max_divisor = 100000000;
+unsigned long long    quantity = 100000;
+
 #include <iostream>
 using namespace std;
 int main()
-{	//#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
-	unsigned long long      dividend = 1000000000000000000; //Change these. (0 to 18446744073709551615)
-	unsigned long long max_remainder = 100000000;
-	unsigned long long      quantity = 100000;
-	//#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
-	
-	unsigned long long wanted_remainder = 0;
+{	unsigned long long wanted_remainder = 0;
 	for(unsigned long long a = 0; a < quantity; a++, wanted_remainder++)
 	{	for(unsigned long long divisor = 1;; divisor++)
 		{	if((dividend % divisor) == wanted_remainder) {cout << wanted_remainder << " = " << dividend << " mod " << divisor << "\n"; break;}
-			if (divisor  > max_remainder)                {cout << wanted_remainder << " = " << dividend << " mod >max_remainder\n";    break;}
+			if (divisor  > max_divisor)                  {cout << wanted_remainder << " = " << dividend << " mod >max_divisor\n";      break;}
 		}
 	}
 }
