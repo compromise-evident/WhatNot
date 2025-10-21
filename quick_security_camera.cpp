2e774efdae046d10078eaf072898c77235270b64
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2025-10-21 12:51:55 -0600

    quick_security_camera: restored time live for ref.

--- quick_security_camera.cpp
+++ quick_security_camera.cpp
@@ -46,25 +46,32 @@ int main()
 	
 	//Begins.
 	system("mkdir Images -p");
+	long long unix_time = time(0);
 	for(long long loop = 0; loop < number_of_images_to_capture; loop++)
-	{	//Makes file name, such as "2025-10-19_20:34:55___Sun_Oct_19_08:34:55_PM_MDT_2025.jpg".
+	{	//Makes file name, such as "2025-10-19_20:34:55___Sun_Oct_19_08:34:55_PM_MDT_2025".
 		system("date --rfc-3339=seconds > temp/military_time");
 		in_stream.open("temp/military_time");
 		char military_time[20] = {'\0'}; for(int a = 0; a < 19; a++) {in_stream.get(military_time[a]);}
 		in_stream.close();
 		military_time[10] = '_';
 		
-		system("date > temp/time");
-		in_stream.open("temp/time");
-		char time[32] = {'\0'}; for(int a = 0; a < 31; a++) {in_stream.get(time[a]);}
+		system("date > temp/civilian_time");
+		in_stream.open("temp/civilian_time");
+		char civilian_time[32] = {'\0'}; for(int a = 0; a < 31; a++) {in_stream.get(civilian_time[a]);}
 		in_stream.close();
-		for(int a = 0; a < 31; a++) {if(time[a] == ' ') {time[a] = '_';}}
+		for(int a = 0; a < 31; a++) {if(civilian_time[a] == ' ') {civilian_time[a] = '_';}}
 		
-		string str = command; str += "Images/"; str += military_time; str += "___"; str += time; str += ".jpg";
+		string str = command; str += "Images/"; str += military_time; str += "___"; str += civilian_time; str += ".jpg";
 		
 		//Takes a photo.
 		system(str.c_str());
+		
+		//Keeps you posted.
 		system("date");
+		cout << (((time(0) - unix_time) / 60) / 60) / 24      << " days, "
+		     << (((time(0) - unix_time) / 60) / 60)      % 24 << " hours, "
+		     <<  ((time(0) - unix_time) / 60)            % 60 << " minutes, "
+		     <<   (time(0) - unix_time)                  % 60 << " seconds\n\n";
 		
 		//Sleeps.
 		for(long long a = 0; a < seconds_to_sleep_between_image_capture; a++) {system("sleep 1");}
