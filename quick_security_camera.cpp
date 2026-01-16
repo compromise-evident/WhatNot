fa79821a0c30970085577fb189e9ab51b405a084
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2026-01-16 15:27:18 -0700

    Allows variable-length time zones (civilian_time).

--- quick_security_camera.cpp
+++ quick_security_camera.cpp
@@ -10,7 +10,6 @@ webcam option in "more USB options" when connecting phone to computer.*/
 #include <fstream>
 #include <iostream>
 using namespace std;
-
 int main()
 {	ifstream in_stream;
 	ofstream out_stream;
@@ -37,8 +36,8 @@ int main()
 		return 0;
 	}
 	
-	string command = "fswebcam -q -r 1920x1080 --no-banner ";
-	if(presence_of_external_webcam == true) {command += " --device /dev/video2 ";}
+	string prefix = "fswebcam -q -r 1920x1080 --no-banner ";
+	if(presence_of_external_webcam == true) {prefix += " --device /dev/video2 ";}
 	
 	
 	
@@ -46,32 +45,33 @@ int main()
 	
 	//Begins.
 	system("mkdir Images -p");
-	long long unix_time = time(0);
+	long long reference_unix_time = time(0);
 	for(long long loop = 0; loop < number_of_images_to_capture; loop++)
-	{	//Makes file name, such as "2025-10-19_20:34:55___Sun_Oct_19_08:34:55_PM_MDT_2025".
+	{	//Gets military time.
 		system("date --rfc-3339=seconds > temp/military_time");
-		in_stream.open("temp/military_time");
-		char military_time[20] = {'\0'}; for(int a = 0; a < 19; a++) {in_stream.get(military_time[a]);}
-		in_stream.close();
-		military_time[10] = '_';
+		in_stream.open("temp/military_time"); char military_time[50] = {'\0'}; in_stream.getline(military_time, 50); in_stream.close();
+		military_time[10] = '_'; military_time[19] = '\0';
 		
+		//Gets civilian time.
 		system("date > temp/civilian_time");
-		in_stream.open("temp/civilian_time");
-		char civilian_time[32] = {'\0'}; for(int a = 0; a < 31; a++) {in_stream.get(civilian_time[a]);}
-		in_stream.close();
-		for(int a = 0; a < 31; a++) {if(civilian_time[a] == ' ') {civilian_time[a] = '_';}}
+		in_stream.open("temp/civilian_time"); char civilian_time[50] = {'\0'}; in_stream.getline(civilian_time, 50); in_stream.close();
+		for(int a = 0; a < 50; a++) {if(civilian_time[a] == ' ') {civilian_time[a] = '_';}}
 		
-		string str = command; str += "Images/"; str += military_time; str += "___"; str += civilian_time; str += ".jpg";
+		//Makes a file name like "Images/2026-01-15_18:03:14___Thu_Jan_15_06:03:14_PM_MST_2026.jpg".
+		string temporal_name = "Images/"; temporal_name += military_time; temporal_name += "___"; temporal_name += civilian_time; temporal_name += ".jpg";
+		
+		//Makes a command like "fswebcam -q -r 1920x1080 --no-banner --device /dev/video2 Images/2026-01-15_18:03:14___Thu_Jan_15_06:03:14_PM_MST_2026.jpg".
+		string command = prefix; command += temporal_name;
 		
 		//Takes a photo.
-		system(str.c_str());
+		system(command.c_str());
 		
 		//Keeps you posted.
 		system("date");
-		cout << (((time(0) - unix_time) / 60) / 60) / 24      << " days, "
-		     << (((time(0) - unix_time) / 60) / 60)      % 24 << " hours, "
-		     <<  ((time(0) - unix_time) / 60)            % 60 << " minutes, "
-		     <<   (time(0) - unix_time)                  % 60 << " seconds\n\n";
+		cout << (((time(0) - reference_unix_time) / 60) / 60) / 24      << " days, "
+		     << (((time(0) - reference_unix_time) / 60) / 60)      % 24 << " hours, "
+		     <<  ((time(0) - reference_unix_time) / 60)            % 60 << " minutes, "
+		     <<   (time(0) - reference_unix_time)                  % 60 << " seconds\n\n";
 		
 		//Sleeps.
 		for(long long a = 0; a < seconds_to_sleep_between_image_capture; a++) {system("sleep 1");}
