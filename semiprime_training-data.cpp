2f01263fc39ab7e9ce945af2c5dc6bdc5067ad49
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2026-06-15 12:17:58 -0600

    Aesthetics.

--- semiprime_training-data.cpp
+++ semiprime_training-data.cpp
@@ -25,19 +25,19 @@ int main()
 	
 	out_stream.open("pq");
 	for(int loops = 0; loops < quantity;)
-	{	p[0] = ((rand() % 9) + 49); for(int a = 1; a < p_length; a++) {p[a] = ((rand() % 10) + 48);}          //Random p.
-		mpz_set_str(randomness, p, 10); mpz_nextprime(prime_p, randomness); mpz_get_str( p, 10, prime_p);     //p made prime.
-		if(p[p_length] != '\0') {p[p_length] = '\0'; continue;}                                               //Restarts if p too long.
+	{	p[0] = ((rand() % 9) + 49); for(int a = 1; a < p_length; a++) {p[a] = ((rand() % 10) + 48);}         //Random p.
+		mpz_set_str(randomness, p, 10); mpz_nextprime(prime_p, randomness); mpz_get_str(p, 10, prime_p);     //p made prime.
+		if(p[p_length] != '\0') {p[p_length] = '\0'; continue;}                                              //Restarts if p too long.
 		for(;;)
-		{	q[0] = ((rand() % 9) + 49); for(; q[0] < p[0];) {q[0] = ((rand() % 9) + 49);}                     //q digit 1 >= p digit 1.
-			for(int a = 1; a < q_length; a++) {q[a] = ((rand() % 10) + 48);}                                  //Random q.
-			mpz_set_str(randomness, q, 10); mpz_nextprime(prime_q, randomness); mpz_get_str( q, 10, prime_q); //q made prime.
-			if(q[q_length] != '\0') {q[q_length] = '\0'; continue;} else {break;}                             //Restarts locally if q too long.
+		{	q[0] = ((rand() % 9) + 49); for(; q[0] < p[0];) {q[0] = ((rand() % 9) + 49);}                    //q digit 1 >= p digit 1.
+			for(int a = 1; a < q_length; a++) {q[a] = ((rand() % 10) + 48);}                                 //Random q.
+			mpz_set_str(randomness, q, 10); mpz_nextprime(prime_q, randomness); mpz_get_str(q, 10, prime_q); //q made prime.
+			if(q[q_length] != '\0') {q[q_length] = '\0'; continue;} else {break;}                            //Restarts locally if q too long.
 		}
-		if(mpz_cmp(prime_p, prime_q) != -1) {continue;}                                                       //Restarts if q <= p.
-		mpz_mul(product, prime_p, prime_q); mpz_get_str(pq, 10, product);                                     //pq made.
-		int length = 0; for(int a = 0; pq[a] != '\0'; a++) {length++;} if(length != pq_length) {continue;}    //Restarts if pq not pq_length.
-		loops++; cout << loops << " of " << quantity << "\n";                                                 //Proceeds only if successful.
+		if(mpz_cmp(prime_p, prime_q) != -1) {continue;}                                                      //Restarts if q <= p.
+		mpz_mul(product, prime_p, prime_q); mpz_get_str(pq, 10, product);                                    //pq made.
+		int length = 0; for(int a = 0; pq[a] != '\0'; a++) {length++;} if(length != pq_length) {continue;}   //Restarts if pq not pq_length.
+		loops++; cout << loops << " of " << quantity << "\n";                                                //Proceeds only if successful.
 		
 		//Saves to file.
 		//###################################################
