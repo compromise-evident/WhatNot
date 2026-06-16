8de52134fda6a6e8a642d6d491edf41841596def
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2026-06-16 16:21:51 -0600

    Add files via upload

---
+++ semiprimes_have_perfect-secrecy-like_nature.cpp
@@ -0,0 +1,84 @@
+//YOUR CONTROLS:                                                                                              Run it: "apt install g++ geany". Open this in Geany. Hit F9 once. F5 to run.
+long long last_few_digits_of_your_semiprime = 423446789115381147; //Must end in 1, 3, 7, or 9.
+long long number_of_ending_digits_to_crack = 4; //Begin at 1 and increase!
+
+
+
+/* Prints which two numbers multiplied recreate n last digits of your semiprime.
+Increase the number of semiprime digits you want recreated.  Listed p*q are the
+plausible ending digits of the factors.  Modify semiprime ending digits as well.
+
+It seems impossible to know any semiprime factor digit, unless you have a factor
+whole.  You cannot eliminate  any factor (plaus)ibilities because  multiplicands
+and multipliers listed are simply all numbers ending in 1, 3, 7, 9. For example,
+given the last few digits of semiprime pq,  primes p are listed as every integer
+1+ but ending in 1, 3, 7, & 9  (as primes should, other than 2 & 5.) Now, for q,
+the same exact numbers are listed as multipliers for p, in a different order. */
+
+#include <iostream>
+using namespace std;
+int main()
+{	long long number_of_ending_digits_via_mod = 1;
+	for(long long a = 0; a < number_of_ending_digits_to_crack; a++) {number_of_ending_digits_via_mod *= 10;}
+	last_few_digits_of_your_semiprime %= number_of_ending_digits_via_mod;
+	
+	long long p_duplicate_watch = 0;
+	long long q_duplicate_watch = 0;
+	
+	long long quantity = 1;
+	for(long long p = 0; p < number_of_ending_digits_via_mod; p++)
+	{	for(long long q = 0; q < number_of_ending_digits_via_mod; q++)
+		{	if(((p % 10) == 1)
+			|| ((p % 10) == 3)
+			|| ((p % 10) == 7)
+			|| ((p % 10) == 9))
+			{	if(((q % 10) == 1)
+				|| ((q % 10) == 3)
+				|| ((q % 10) == 7)
+				|| ((q % 10) == 9))
+				{	long long product = (p * q);
+					product %= number_of_ending_digits_via_mod;
+					
+					if(product == last_few_digits_of_your_semiprime)
+					{	long long max_length_reference = (number_of_ending_digits_via_mod - 1);
+						max_length_reference /= 10;
+						
+						cout << "possibility_";
+						for(int temp = max_length_reference, a = 0; a < (number_of_ending_digits_to_crack - 1); a++)
+						{	if(quantity <= temp) {cout << "_";}
+							temp /= 10;
+						}
+						cout << quantity << ":     ...";
+						
+						for(int temp = max_length_reference, a = 0; a < (number_of_ending_digits_to_crack - 1); a++)
+						{	if(p <= temp) {cout << "0";}
+							temp /= 10;
+						}
+						cout << p << " * ...";
+						
+						for(int temp = max_length_reference, a = 0; a < (number_of_ending_digits_to_crack - 1); a++)
+						{	if(q <= temp) {cout << "0";}
+							temp /= 10;
+						}
+						cout << q << "     = ..." << product << " (semiprime)\n";
+						
+						p_duplicate_watch += p;
+						q_duplicate_watch += q;
+						quantity++;
+						
+						break;
+					}
+				}
+			}
+		}
+	}
+	
+	if(p_duplicate_watch != q_duplicate_watch) {cout << "\n\nFAILED! Cryptographic semiprimes end in 1, 3, 7, or 9."; return 0;}
+	
+	cout << "\n\n\nQuantity is always 4*10^n because 40% of numbers end in 1, 3, 7, 9."
+	     << "\n\n~Half are duplicates, as in a*b and b*a."
+	     << "\n\np and q sums are always 2*10^n, equal, and"
+	     << "\ntwice the digit length of possibilities;"
+	     << "\nsum(all p) = " << p_duplicate_watch 
+	     << "\nsum(all q) = " << q_duplicate_watch;
+}
---
+++ try_RSA.cpp
@@ -0,0 +1,55 @@
+//YOUR CONTROLS:                                                                Run it: "apt install g++ geany libgmp-dev". Open the .cpp in Geany.
+char semiprime[100001] = {"4095075870816883"};                                  //Append "-lgmp" to Geany's compile & build commands. Hit F9 once. F5 to run.
+int candidate_factor_digit_length = 8;
+char known_first_digits_of_a_factor[50001] = {""};
+//You can put one or more digits between the quotes.
+
+
+
+/*Attempt factorization of semiprimes up to 100k digits
+long. You can provide n known first digits of a factor.*/
+
+#include <fstream>
+#include <gmp.h>
+#include <iostream>
+using namespace std;
+int main()
+{	ifstream in_stream;
+	ofstream out_stream;
+	
+	srand(time(0));
+	int semiprime_digit_length = 0; for(int a = 0;                      semiprime[a] != '\0'; a++) {semiprime_digit_length++;}
+	int known_digits_length    = 0; for(int a = 0; known_first_digits_of_a_factor[a] != '\0'; a++) {known_digits_length   ++;}
+	char candidate_factor[50001] = {'\0'};
+	for(int a = 0; a < known_digits_length; a++) {candidate_factor[a] = known_first_digits_of_a_factor[a];}
+	for(int a = known_digits_length; a < candidate_factor_digit_length; a++) {candidate_factor[a] = ((rand() % 10) + 48);}
+	cout << "\nFactoring " << semiprime_digit_length << "-digit semiprime.\nLooking for " << candidate_factor_digit_length << "-digit prime.\n";
+	
+	mpz_t dividend        ; mpz_init(dividend        );
+	mpz_t divisor         ; mpz_init(divisor         );
+	mpz_t remainder       ; mpz_init(remainder       );
+	mpz_t wanted_remainder; mpz_init(wanted_remainder);
+	int zero = 0;
+	mpz_set_si (wanted_remainder, zero );
+	mpz_set_str(dividend, semiprime, 10);
+	
+	for(int temp = (candidate_factor_digit_length - 1);;)
+	{	//Modifies candidate_factor and ensures it does not begin with 0, and that it ends in 1, 3, 7, or 9.
+		//Only one randomly-chosen digit is changed at a time (on the candidate factor.)
+		int random_index = (rand() % (candidate_factor_digit_length - known_digits_length)); random_index += known_digits_length;
+		     if(random_index ==    0) {candidate_factor[           0] = ((rand() %  9) + 49);}
+		else if(random_index  < temp) {candidate_factor[random_index] = ((rand() % 10) + 48);}
+		else
+		{	candidate_factor[temp] = (rand() % 4);
+			if((candidate_factor[temp] % 2) == 0) {candidate_factor[temp] += 7;}
+			candidate_factor[temp] += 48;
+		}
+		
+		mpz_set_str(divisor, candidate_factor, 10); mpz_mod(remainder, dividend, divisor ); //Mod operation.
+		int comparison_of_wanted_remainder_with_zero = mpz_cmp(wanted_remainder, remainder);
+		if(comparison_of_wanted_remainder_with_zero == 0)
+		{	out_stream.open("FACTOR", ios::app); out_stream << candidate_factor << "\n"; out_stream.close(); //Saves factor to file.
+			cout << "\nDone! Prime appended to file \"FACTOR\":\n\n" << candidate_factor << "\n\n"; return 0;
+		}
+	}
+}
