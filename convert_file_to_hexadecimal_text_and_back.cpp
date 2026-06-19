16b276567ad1e945ef63b8f49127e09129a723ec
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2026-06-19 17:34:41 -0600

    Add files via upload

--- convert_file_to_hexadecimal_text_and_back.cpp
+++ convert_file_to_hexadecimal_text_and_back.cpp
@@ -6,6 +6,7 @@ Recipients can use this tool to undo it. */
 
 #include <fstream>
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {	int raw_byte;
@@ -17,12 +18,11 @@ int main()
 	     << "\n(2) Hex back to bin."
 	     << "\n\nOption: "; int o; cin >> o;
 	
-	//Gets path, fixes it if dropped.
-	cout << "Drop/enter file:\n";
-	char path[100000] = {'\0'}; cin.getline(path, 100000); if(path[0] == '\0') {cin.getline(path, 100000);}
-	if(path[0] == '\'') {for(int bm = 0, a = 0; a < 100000; a++) {if(path[a] != '\'') {path[bm] = path[a]; if(path[bm] == '\\') {path[bm] = '\'';} bm++;}}}
-	for(int a = 99999; a >= 0; a--) {if(path[a] != '\0') {if(path[a] == ' ') {path[a] = '\0';} break;}}
-	in_stream.open(path); if(in_stream.fail() == true) {cout << "\nNo path " << path << "\n"; return 0;} in_stream.close();
+	//Gets path (files & folders).
+	std::cout << "\nDrop/enter file:\n";
+	std::string path; std::getline(std::cin, path); if(path[0] == '\0') {std::getline(std::cin, path);}
+	if(path[0] == '\'') {path.erase(0, 1); path.pop_back(); path.pop_back();} //Fixes path if drag-n-dropped.
+	in_stream.open(path); if(!in_stream) {std::cout << "\nNo path " << path << "\n"; return 1;} in_stream.close();
 	
 	//Bin to hex.
 	if(o == 1)
